7eeaedecac17e9d0968feacb73280ec3d80c028b
Dan Dwyer <dadwyer@lbl.gov>
2015-03-26 13:18:41 -0700

    Minor mods for ROOT v6 compatibility.

--- Script/loadClasses.C
+++ Script/loadClasses.C
@@ -1,19 +1,21 @@
 {
     TString include = ".include ";
     TString load = ".L ";
+    TString pwd = gSystem->pwd();
+    pwd = pwd + '/';
 
     TString prefix = "../DAQ";
-    gROOT->ProcessLine( include + prefix );
+    gROOT->ProcessLine( include + pwd + prefix );
     gROOT->ProcessLine( load + prefix + "/Unpacker.cc+" );
 
     prefix = "../Event";
-    gROOT->ProcessLine( include + prefix );
+    gROOT->ProcessLine( include + pwd + prefix );
     gROOT->ProcessLine( load + prefix + "/DAQEvent.cc+" );
     gROOT->ProcessLine( load + prefix + "/WFAnalyzer.cc+" );
     // gROOT->ProcessLine( load + prefix + "/RecEvent.cc+" );
 
     prefix = "../Viewer";
-    gROOT->ProcessLine( include + prefix );
+    gROOT->ProcessLine( include + pwd + prefix );
     gROOT->ProcessLine( load + prefix + "/ViewWindow.cc+" );
     gROOT->ProcessLine( load + prefix + "/InfoWindow.cc+" );
     gROOT->ProcessLine( load + prefix + "/ControlWindow.cc+" );
--- Script/show.C
+++ Script/show.C
@@ -1,6 +1,6 @@
 {
     gROOT->Reset();
-    gROOT->ProcessLine(".x loadClasses.C" );
+    gROOT->ProcessLine(".x loadClasses.C");
 
     // x = new DAQEvent("~/Projects/PROSPECT/PROSPECT2/PROSPECT2/DAQ/test.root");
     // x.GetEntry(0);
@@ -8,6 +8,6 @@
     // x.GetEntry(x.nEvents-1);
     // x.PrintInfo(1);
 
-    GuiController *gc = new GuiController(gClient->GetRoot(), 900, 900);
+    gROOT->ProcessLine("GuiController *gc = new GuiController(gClient->GetRoot(), 900, 900);");
 
 }
